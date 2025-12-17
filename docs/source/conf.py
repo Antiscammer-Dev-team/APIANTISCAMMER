f43975e90c2177bcd9acdddd5ce76d2f1a7443a4
Antiscammer-Dev-team <maicusryan@gmail.com>
2025-12-16 20:22:16 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AntiscammerAPI'
+copyright = '2025, Antiscam dev team'
+author = 'Antiscam dev team'
 
 release = '0.1'
 version = '0.1.0'
